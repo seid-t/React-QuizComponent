1a0aafd3d5fc0f998c766065eeccc135ae8fc0cc
STABM42 <seid.tabakovic@realdolmen.com>
2019-03-19 16:43:36 +0100

    Handling Events completed

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
+    handleClick(buttonText) {
+        if(buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler();
+        }
+    }
+
     render() {
         return(
             <main>
@@ -13,7 +19,9 @@ class QuizQuestion extends Component {
                 </section>
                 <section className="buttons">
                     <ul>
-                        <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                        {this.props.quiz_question.answer_options.map((answer_options, index) => {
+                            return <QuizQuestionButton clickHandler = {this.handleClick.bind(this)} key = {index} button_text = {answer_options}/>
+                        })}
                     </ul>
                 </section>
             </main>
